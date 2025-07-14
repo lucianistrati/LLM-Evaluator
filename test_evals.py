a794fb26a8280d3bd079a4a85cf5393805d75a56
Lucian Istrati <lucianistrati@yahoo.com>
2025-07-14 10:38:21 +0400

    Update test_evals.py

--- test_evals.py
+++ test_evals.py
@@ -1,36 +1,34 @@
-from deepeval.metrics import GEval, HallucinationMetric
+# test_evals.py
+
+from deepeval.metrics import GEval, HallucinationMetric, AnswerRelevancyMetric
 from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 from deepeval import assert_test
 
 def test_hallucination():
-    hallucination_metric = HallucinationMetric(minimum_score=
-0.5
-)
+    hallucination_metric = HallucinationMetric(minimum_score=0.5)
     test_case = LLMTestCase(
-    	input="What if these shoes don't fit?", 
-      actual_output="We offer a 30-day full refund at no extra costs.", 
-      context=["All customers are eligible for a 30 day full refund at no extra costs."]
-     )
+        input="What if these shoes don't fit?",
+        actual_output="We offer a 30-day full refund at no extra costs.",
+        context=["All customers are eligible for a 30-day full refund at no extra costs."]
+    )
     assert_test(test_case, [hallucination_metric])
 
 def test_relevancy():
-    answer_relevancy_metric = AnswerRelevancyMetric(minimum_score=
-0.5
-)
+    relevancy_metric = AnswerRelevancyMetric(minimum_score=0.5)
     test_case = LLMTestCase(
-    	input="What does your company do?", 
-      actual_output="Our company specializes in cloud computing"
-     )
+        input="What does your company do?",
+        actual_output="Our company specializes in cloud computing."
+    )
     assert_test(test_case, [relevancy_metric])
-    
+
 def test_humor():
     funny_metric = GEval(
-    		name="Humor",
+        name="Humor",
         criteria="How funny it is",
         evaluation_params=[LLMTestCaseParams.ACTUAL_OUTPUT]
     )
     test_case = LLMTestCase(
-    	input="Write me something funny related to programming", 
-      actual_output="Why did the programmer quit his job? Because he didn't get arrays!"
+        input="Write me something funny related to programming",
+        actual_output="Why did the programmer quit his job? Because he didn't get arrays!"
     )
     assert_test(test_case, [funny_metric])
